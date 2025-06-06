17418c78d441230e5bceda84d0d768d0247b7979
DaWebAlpha <tribillio@gmail.com>
2025-06-06 11:46:19 -0700

    block-scope

---
+++ lesson2/index2.js
@@ -0,0 +1,7 @@
+//Variable Names
+// 1) names only contain letters, numbers, symbol $ _
+// 2) first character must not be a number
+
+var first1Name;
+var second1Name;
+var camelCase$; 
--- lesson3/index3.js
+++ lesson3/index3.js
@@ -1,1 +1,22 @@
 //VARIABLE NAMES CHALLENGE
+// Fix the variable names below, and log them out to the console!
+/*
+var firstname = "Reed";
+
+var sur-name = "Barger";
+
+var 1000 = "one thousand";
+
+*/
+
+//use OF camelCase
+var firstName = "Reed";
+
+//- is not allowed in a variable name
+var surName = "Barger";
+
+//Number cannot start a variable name
+var oneThousand = "one thousand";
+
+
+
