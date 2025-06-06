0c31cbd77bc49711b925cc06471cb4cf57f2fee5
DaWebAlpha <tribillio@gmail.com>
2025-06-06 12:09:49 -0700

    template literals

---
+++ lesson7/index7.js
@@ -0,0 +1,18 @@
+let name = "Abradu";
+
+const message = `My name is ${name}`;
+
+console.log(message);
+
+//// Challenge: Help Darth Vader express himself to his son!
+/* const mySon = "Luke";
+const parentalStatus = "father";
+const message1 = "mySon, I am your parentalStatus";
+console.log(message1);
+*/
+
+// Challenge: Help Darth Vader express himself to his son!
+const mySon = "Luke";
+const parentalStatus = "father";
+const message1 = `${mySon}, I am your ${parentalStatus}`;
+console.log(message1);
